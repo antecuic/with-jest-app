34c122e9c9f02a355a11e30064b2f6aa209fd3c6
antecuic <antecuic00@gmail.com>
2022-09-17 12:37:47 +0200

    added tests for form & form elements

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,14 +1,76 @@
-import { render, screen } from '@testing-library/react'
-import Home from '@/pages/index'
+import { fireEvent, render, screen } from "@testing-library/react";
+import Home from "@/pages/index";
 
-describe('Home', () => {
-  it('renders a heading', () => {
-    render(<Home />)
+describe("Home", () => {
+  it("renders properly form", () => {
+    render(<Home />);
 
-    const heading = screen.getByRole('heading', {
-      name: /welcome to next\.js!/i,
-    })
+    const form = screen.getByTestId("calc-roman-id");
+    expect(form).toBeInTheDocument();
 
-    expect(heading).toBeInTheDocument()
-  })
-})
+    const integerLabel = screen.getByText(/Integer:/i);
+    expect(integerLabel).toBeInTheDocument();
+
+    const input = screen.getByLabelText(/Integer:/i);
+    expect(input).toBeInTheDocument();
+    expect(input).toHaveAttribute("type", "number");
+    expect(input).toHaveAttribute("min", "1");
+    expect(input).toHaveAttribute("max", "1000");
+
+    const button = screen.getByRole("button", { name: "Calculate" });
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveAttribute("type", "submit");
+  });
+
+  it("button should be disabled if input value is less than 1 ", () => {
+    render(<Home />);
+
+    const input = screen.getByLabelText(/Integer:/i);
+
+    const button = screen.getByRole("button", { name: "Calculate" });
+
+    fireEvent.change(input, { target: { value: -2 } });
+    expect(button).toHaveProperty("disabled", true);
+
+    const note = screen.getByRole("p", {
+      name: "Integer must be between 1 and 1000",
+    });
+
+    expect(note).toBeInTheDocument();
+  });
+
+  it("button should be disabled if input value is greater than 1000", () => {
+    render(<Home />);
+
+    const input = screen.getByLabelText(/Integer:/i);
+
+    const button = screen.getByRole("button", { name: "Calculate" });
+
+    fireEvent.change(input, { target: { value: 1003 } });
+    expect(button).toHaveProperty("disabled", true);
+
+    const note = screen.getByRole("p", {
+      name: "Integer must be between 1 and 1000",
+    });
+
+    expect(note).toBeInTheDocument();
+  });
+
+  it("button should be enabled input value is between 1 and 1000", () => {
+    render(<Home />);
+
+    const input = screen.getByLabelText(/Integer:/i);
+
+    const button = screen.getByRole("button", { name: "Calculate" });
+
+    fireEvent.change(input, { target: { value: "432" } });
+
+    expect(button).toHaveProperty("disabled", false);
+
+    const note = screen.getByRole("p", {
+      name: "Integer must be between 1 and 1000",
+    });
+
+    expect(note).not.toBeInTheDocument();
+  });
+});
